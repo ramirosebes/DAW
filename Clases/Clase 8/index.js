bf9b960ae01077dc67e0827bdf0d18cb70807d6a
Ramiro Sebes <ramirosebes@gmail.com>
2023-06-13 16:03:07 -0300

    avance media query y anotaciones clase 8

---
+++ Clases/Clase 8/index.js
@@ -0,0 +1,38 @@
+/*----------Ejercicio 1----------*/
+// var suma = function (n1, n2) {
+//     return n1 + n2;
+// }
+
+// var r1 = suma(5, 8);
+// var r2 = suma(-4, -6);
+// var r3 = suma(1/2, 2);
+// var r4 = suma(1, true);
+// console.log(r1, r2, r3, r4);
+
+// ctrl k + c --> para comentar seleccion
+
+/*----------ejercicio 2----------*/
+// var suma = function (n1, n2){
+//     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+//         alert('Numero no valido')
+//         return NaN;
+//     }
+//     return n1 + n2;
+// }
+
+// var r1 = suma(5, 8);
+// var r2 = suma(-4, -6);
+// var r3 = suma(2, '2');  
+// var r4 = suma(1, true);
+// console.log(r1, r2, r3, r4);
+
+/*----------Ejercicio 3----------*/
+// var validateInteger = function (num) {
+//     if (num - parseInt(num) > 0){
+        
+//     }
+// }
+
+//Number. isInteger();
+
+/*----------Ejercicio 4----------*/
